ebade5662075fb82a26ba2500a43f2cbd5354c49
Dev <flashytesting@gmail.com>
2023-02-16 12:27:47 +0530

    Add dummy date && about page routing

--- blog_post/views.py
+++ blog_post/views.py
@@ -1,10 +1,35 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for
 
 views = Blueprint("views", __name__)
 
+dummy_data = [
+    {
+        "author": "Dev",
+        "title":  "Some post tilte",
+        "description": "Some post description",
+        "date": "The creation date"
+    },
+    {
+        "author": "Udhbhav",
+        "title":  "Some other post tilte",
+        "description": "Some other post description",
+        "date": "The new creation date"
+    },
+    {
+        "author": "Aman",
+        "title":  "Some another post tilte",
+        "description": "Some another post description",
+        "date": "The latest creation date"
+    }
+]
 
 @views.route("/")
 @views.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", posts=dummy_data)
+
+
+@views.route("/about")
+def about():
+    return render_template("about.html")
 
