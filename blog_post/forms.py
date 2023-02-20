96310e56d2072ff4eb8f5dde343ee173f2b23897
Dev <flashytesting@gmail.com>
2023-02-20 19:43:42 +0530

    Add ValidationError on similar username/email

--- blog_post/forms.py
+++ blog_post/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from .models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -20,6 +21,18 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField("Sign up")
 
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("Username already in use.")
+
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("Email already in use.")
+
+
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[
             DataRequired(),
@@ -28,3 +41,4 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()])
     remember = BooleanField("Remember me")
     submit = SubmitField("Login")
+
