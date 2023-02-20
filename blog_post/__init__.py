0e35c3d40cfb01b62df3ebc9d3fb27889d49c377
Dev <flashytesting@gmail.com>
2023-02-20 18:58:10 +0530

    Add database

--- blog_post/__init__.py
+++ blog_post/__init__.py
@@ -1,14 +1,21 @@
 from flask import Flask
 from dotenv import load_dotenv
-from os import getenv
+from os import getenv, path
+from flask_sqlalchemy import SQLAlchemy
 
 load_dotenv()
 SECRET_KEY = getenv("SECRET_KEY")
 
+db = SQLAlchemy()
+DB_NAME = "site.db"
 
 def create_app():
     app = Flask(__name__)
+
     app.config["SECRET_KEY"] = SECRET_KEY
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -16,5 +23,15 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
+    from .models import User, Post
+
+    create_database(app)
+
     return app
+
+
+def create_database(app):
+    if not path.exists("blog_post/" + DB_NAME):
+        db.create_all(app=app)
+        print("Created Database")
 
