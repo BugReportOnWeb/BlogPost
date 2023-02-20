d04ddb709718f7c578cd0cd13c710ac66762a1ba
Dev <flashytesting@gmail.com>
2023-02-20 19:43:10 +0530

    Update user authentication

--- blog_post/auth.py
+++ blog_post/auth.py
@@ -3,19 +3,24 @@ from .forms import RegistrationForm, LoginForm
 from .models import User, Post
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, current_user, login_required
 
 auth = Blueprint("auth", __name__)
 
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("views.home"))
+
     form = LoginForm()
 
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-
+        
         if user:
             if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
                 flash("You have been logged in!", category="success")
                 return redirect(url_for("views.home"))
             else:
@@ -28,26 +33,26 @@ def login():
 
 @auth.route("/register", methods=["GET", "POST"])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("views.home"))
+
     form = RegistrationForm()
 
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
+        hashed_password = generate_password_hash(form.password.data, method="sha256")
+        new_user = User(email=form.email.data, username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
 
-        if not user:
-            hashed_password = generate_password_hash(form.password.data, method="sha256")
-            new_user = User(email=form.email.data, username=form.username.data, password=hashed_password)
-            db.session.add(new_user)
-            db.session.commit()
-
-            flash(f"Account created for {form.username.data}!", category="success")
-            return redirect(url_for("views.home"))
-
-        flash("Email already registered!", category="danger")
+        flash(f"Account created for {form.username.data}!", category="success")
+        return redirect(url_for("views.home"))
         
     return render_template("register.html", form=form)
 
 
 @auth.route("/logout")
+@login_required
 def logout():
-    return redirect(url_for("views.home"))
+    logout_user()
+    return redirect(url_for("auth.login"))
 
