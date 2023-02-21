12f30c078256f8e4ec5ef4c1e39025ae977c4857
Dev <flashytesting@gmail.com>
2023-02-21 19:49:48 +0530

    Fix default user profile path

--- blog_post/models.py
+++ blog_post/models.py
@@ -8,7 +8,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), nullable=False, unique=True)
     email = db.Column(db.String(120), nullable=False, unique=True)
     password = db.Column(db.String(60), nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
+    image_file = db.Column(db.String(20), nullable=False, default="default.png")
     posts = db.relationship("Post", backref="author", lazy=True)
 
     def __repr__(self):
