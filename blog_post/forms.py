07ef714b786f0050966d9c80f82fcddffd05459b
Dev <flashytesting@gmail.com>
2023-02-21 19:49:19 +0530

    Add update account form

--- blog_post/forms.py
+++ blog_post/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from .models import User
+from flask_login import current_user
 
 
 class RegistrationForm(FlaskForm):
@@ -42,3 +43,28 @@ class LoginForm(FlaskForm):
     remember = BooleanField("Remember me")
     submit = SubmitField("Login")
 
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField("Username", validators=[
+            DataRequired(),
+            Length(min=2, max=20)
+        ])
+    email = StringField("Email", validators=[
+            DataRequired(),
+            Email()
+        ])
+    submit = SubmitField("Update")
+
+
+    def validate_username(self, username):
+        if username.data != current_user.email:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError("Username already in use.")
+
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError("Email already in use.")
