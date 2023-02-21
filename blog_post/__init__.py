982cb3797a389f275b91d1e17f4313a22d03ffb8
Dev <flashytesting@gmail.com>
2023-02-21 18:33:59 +0530

    Update login category message && add account page

--- blog_post/__init__.py
+++ blog_post/__init__.py
@@ -31,6 +31,7 @@ def create_app():
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
     login_manager.init_app(app)
+    login_manager.login_message_category = "info"
 
 
     @login_manager.user_loader
--- blog_post/views.py
+++ blog_post/views.py
@@ -6,23 +6,24 @@ views = Blueprint("views", __name__)
 dummy_data = [
     {
         "author": "Dev",
-        "title":  "Some post tilte",
+        "title":  "Some post title",
         "content": "Some post description",
         "date": "The creation date"
     },
     {
         "author": "Udhbhav",
-        "title":  "Some other post tilte",
+        "title":  "Some other post title",
         "content": "Some other post description",
         "date": "The new creation date"
     },
     {
         "author": "Aman",
-        "title":  "Some another post tilte",
+        "title":  "Some another post title",
         "content": "Some another post description",
         "date": "The latest creation date"
     }
 ]
+
 
 @views.route("/")
 @views.route("/home")
@@ -34,4 +35,10 @@ def home():
 @views.route("/about")
 def about():
     return render_template("about.html")
+
+
+@views.route("/account")
+@login_required
+def account():
+    return render_template("account.html")
 
