6d28bcb69001ad86cf01ce17600f1388d7a0e57d
Dev <flashytesting@gmail.com>
2023-02-22 18:51:16 +0530

    Add file field for uploading profile picture

--- blog_post/forms.py
+++ blog_post/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from .models import User
@@ -53,11 +54,14 @@ class UpdateAccountForm(FlaskForm):
             DataRequired(),
             Email()
         ])
+    picture = FileField("Update Profile Picture", validators=[
+        FileAllowed(["png", "jpg", "jpeg"])
+        ])
     submit = SubmitField("Update")
 
 
     def validate_username(self, username):
-        if username.data != current_user.email:
+        if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
             if user:
                 raise ValidationError("Username already in use.")
