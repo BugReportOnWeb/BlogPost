9048ee0fdc4cf3ea9619e09434fd06844a3b6368
Dev <flashytesting@gmail.com>
2023-02-22 18:53:21 +0530

    Add uploaded picture uri to db and change picture

--- blog_post/views.py
+++ blog_post/views.py
@@ -2,6 +2,10 @@ from flask import Blueprint, render_template, url_for, request, flash, redirect,
 from flask_login import login_required, current_user
 from .forms import UpdateAccountForm
 from . import db
+from PIL import Image
+
+import secrets
+import os
 
 views = Blueprint("views", __name__)
 
@@ -39,12 +43,31 @@ def about():
     return render_template("about.html")
 
 
+def save_picture(picture):
+    random_hex = secrets.token_hex(8);
+    _, file_extension = os.path.splitext(picture.filename) 
+    picture_name = random_hex + file_extension
+    picture_path = os.path.join(views.root_path, "static/profile_pics", picture_name)
+    
+    output_size = (125, 125)
+    resized_image = Image.open(picture)
+    resized_image.thumbnail(output_size)
+
+    resized_image.save(picture_path)
+
+    return picture_name
+
+
 @views.route("/account", methods=["GET", "POST"])
 @login_required
 def account():
     form = UpdateAccountForm()
 
     if form.validate_on_submit():
+        if form.picture.data:
+            image_file = save_picture(form.picture.data)
+            current_user.image_file = image_file
+
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
