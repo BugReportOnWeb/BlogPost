e173b0d3c4470e215da6e9cfc55b7265d57a6cee
Dev <flashytesting@gmail.com>
2023-02-27 00:37:30 +0530

    Add SMTP configs for password reset link

--- blog_post/__init__.py
+++ blog_post/__init__.py
@@ -3,20 +3,35 @@ from dotenv import load_dotenv
 from os import getenv, path
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_mail import Mail
 
 load_dotenv()
 SECRET_KEY = getenv("SECRET_KEY")
+EMAIL_USER = getenv("EMAIL_USER")
+EMAIL_PASS = getenv("EMAIL_PASS")
 
 db = SQLAlchemy()
+mail = Mail()
 DB_NAME = "site.db"
 
 def create_app():
     app = Flask(__name__)
 
     app.config["SECRET_KEY"] = SECRET_KEY
+
+    # Database specific config
     app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    # Mail specific config
+    app.config["MAIL_SERVER"] = "smtp.gmail.com"
+    app.config["MAIL_PORT"] = 587
+    app.config["MAIL_USE_TLS"] = True
+    app.config["MAIL_USERNAME"] = EMAIL_USER
+    app.config["MAIL_PASSWORD"] = EMAIL_PASS
+
     db.init_app(app)
+    mail.init_app(app) 
 
     from .views import views
     from .auth import auth
