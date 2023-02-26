7d40819e8dd0e35a2e77eaa51df236ab36f18ecc
Dev <flashytesting@gmail.com>
2023-02-27 00:38:57 +0530

    Add forms for password reset

--- blog_post/forms.py
+++ blog_post/forms.py
@@ -9,8 +9,7 @@ from flask_login import current_user
 class RegistrationForm(FlaskForm):
     username = StringField("Username", validators=[
             DataRequired(),
-            Length(min=2, max=20)
-        ])
+            Length(min=2, max=20) ])
     email = StringField("Email", validators=[
             DataRequired(),
             Email()
@@ -78,3 +77,27 @@ class PostForm(FlaskForm):
     title = StringField("Title", validators=[DataRequired()])
     content = TextAreaField("Content", validators=[DataRequired()])
     submit = SubmitField("Post")
+
+
+class RequestResetForm(FlaskForm):
+    email = StringField("Email", validators=[
+            DataRequired(),
+            Email()
+        ])
+    submit = SubmitField("Request Password Reset")
+
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if not user:
+            raise ValidationError("There is no account with that email. You must register first.")
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField("Confirm Password", validators=[
+            DataRequired(),
+            EqualTo("password")
+        ])
+    submit = SubmitField("Reset Password")
+
